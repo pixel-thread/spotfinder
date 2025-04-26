66935dcff026765890c02b6b7fd02e781b2d0a82
JyrwaJR <jyrwaboys@gmail.com>
2025-04-26 16:15:00 +0530

    add recent user parkings

--- src/libs/endpoints/user/index.ts
+++ src/libs/endpoints/user/index.ts
@@ -1,8 +1,9 @@
 import { EndpointT } from '~/src/types/endpoints';
 
-type UserEndpointKeys = 'PUT_USER' | 'PUT_USER_PASSWORD';
+type UserEndpointKeys = 'PUT_USER' | 'PUT_USER_PASSWORD' | 'GET_USER_RECENT_BOOKINGS';
 
 export const USER_ENDPOINT: EndpointT<UserEndpointKeys> = {
   PUT_USER: '/user/:id',
   PUT_USER_PASSWORD: '/user/:id/password',
+  GET_USER_RECENT_BOOKINGS: '/user/:id/recent-bookings',
 };
